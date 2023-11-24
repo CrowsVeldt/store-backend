0673016054fdb68a5b203c0cc0934a7139fef05b
CrowsVeldt <zechy.mahler@gmail.com>
2023-11-24 08:55:50 +0200

    update allowed origins

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://192.168.1.161:5173",
+  "https://store-frontend-694w.onrender.com/",
 ];
 
 module.exports = allowedOrigins;
